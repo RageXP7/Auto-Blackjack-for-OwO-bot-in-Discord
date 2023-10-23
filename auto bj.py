71a8eba5714e213d7ba30452efcff778fcc6b9e2
RageXP7 <148817908+RageXP7@users.noreply.github.com>
2023-10-24 02:57:56 +0530

    auto bj.py

--- auto bj.py
+++ auto bj.py
@@ -3,118 +3,104 @@ import time
 import json
 import random
 from playsound import playsound
-import emoji
+
+# Configuration
 start = True
 game = True
 list1 = []
-clock = [9,10,11]
+clock = [9, 10, 11]
 default = 500
 bet = default
-bets = [500,500,500,500,500,500,500,1000]
+bets = [500, 500, 500, 500, 500, 500, 500, 1000]
+
+#ToBeFilledByYou
 username = ""
+discord_channel_url = ""
+header = {'authorization': ""}
+audio_path = ""
 
-def bj(choice) :
-    if choice == "punch" :
-        punch()
-    elif choice == "halt" :
-        halt()
-    elif choice == "lost" or choice == "won" :
+# Functions for actions
+def bj(choice):
+    if choice == "punch":
+        action("👊", "react_Punch")
+    elif choice == "halt":
+        action("🛑", "react_Halt")
+    elif choice in ["lost", "won"]:
         gameover(choice)
-        
-        
-def punch() :
-    global r
-    global react_Punch
-    if react_Punch == False :
-        print("PUNCH : ",react_Punch )
-        r = requests.put("https://discord.com/api/v9/channels/1094258368245411942/messages/{}/reactions/%F0%9F%91%8A/%40me?location=Message&burst=false".format(id),headers=header )
-        react_Punch = True
-    else :
-        print("PUNCH : ",react_Punch )
-        r = requests.delete("https://discord.com/api/v9/channels/1094258368245411942/messages/{}/reactions/%F0%9F%91%8A/%40me?location=Message".format(id),headers=header )
-        react_Punch = False
-def halt() :
-    global r
-    global react_Halt
-    if react_Halt == False :
-        print("HALT : ", react_Halt)
-        r = requests.put("https://discord.com/api/v9/channels/1094258368245411942/messages/{}/reactions/%F0%9F%9B%91/%40me?location=Message&burst=false".format(id),headers=header )
-        react_Halt = True
-    else :
-        print("HALT : ", react_Halt)
-        r = requests.delete("https://discord.com/api/v9/channels/1094258368245411942/messages/{}/reactions/%F0%9F%9B%91/%40me?location=Message".format(id),headers=header )
-        react_Halt = False
-        
-def gameover(x) :
-    global bet
-    if x == "lost" :
-        bet = bet*2
-        if bet > 120001 :
-            bet = default
-    elif x == "won" :
-        bet = random.choice(bets)
-    elif x == "draw" :
-        print("draw")
-        
-def check() :
-    global start
-    l = requests.get("https://discord.com/api/v9/channels/1094258368245411942/messages", headers = header )
-    log = json.loads(l.text)
-    log = log[0]
-    result = log['content']
-    print(result)
-    if "hajime" in result :
-        global game
-        start = True 
-        game = True
-        
-header = {
-        'authorization': "<>"
-        }     
 
-while(start == True) :        
-    if game == True :
+def action(emoji, reaction_variable):
+    global r
+    global game
+    if not globals()[reaction_variable]:
+        print(f"{emoji} : {globals()[reaction_variable]}")
+        r = requests.put(f"{discord_channel_url}/{id}/reactions/{emoji}/@me?location=Message&burst=false", headers=header)
+        globals()[reaction_variable] = True
+    else:
+        print(f"{emoji} : {globals()[reaction_variable]}")
+        r = requests.delete(f"{discord_channel_url}/{id}/reactions/{emoji}/@me?location=Message", headers=header)
+        globals()[reaction_variable] = False
+
+def gameover(x):
+    global bet
+    if x == "lost":
+        bet = bet * 2
+        if bet > 120001:
+            bet = default
+    elif x == "won":
+        bet = random.choice(bets)
+    elif x == "draw":
+        print("draw")
+
+# Check for "hajime" to start the game
+def check_start():
+    l = requests.get(discord_channel_url, headers=header)
+    log = json.loads(l.text)
+    result = log[0]['content']
+    return "hajime" in result
+
+while start:
+    if game:
         react_Punch = False
         react_Halt = False
         time.sleep(random.choice(clock))
-        message = {
-        'content' : "owo bj {}".format(bet)
-        }
-        r = requests.post("https://discord.com/api/v9/channels/1094258368245411942/messages", data = message, headers = header )
-        time.sleep(3) 
-    print(game)  
-    l = requests.get("https://discord.com/api/v9/channels/1094258368245411942/messages", headers = header )
+        message = {'content': f"owo bj {bet}"}
+        r = requests.post(discord_channel_url, data=message, headers=header)
+        time.sleep(3)
+
+    l = requests.get(discord_channel_url, headers=header)
     log = json.loads(l.text)
     result = str(log[0])
-    print(result)
-    if game == True :
-        for num in range(8,27) :
-            list1.append(result[num])
+    
+    if game:
+        list1 = result[8:27]
+
     game = False
     id = "".join(list1)
-    print(id)
-    if "human!" in result or "owo bj" in result :
-        print("Caught Redhanded - ABORTING ")
-        playsound("./DANGER.mp3")
+
+    if "human!" in result or f"owo bj {bet}" in result:
+        print("Caught Redhanded - ABORTING")
+        playsound(f"{audio_path}")
         start = False
-        while(start == False) :
-            check()
+        while not start:
+            check_start()
             time.sleep(5)
-            
-    for i in range(1,16) :
+
+    for i in range(1, 16):
         if f"'{username} `[{i}]`" in result or f"'{username} `[{i}]*`" in result:
             ch = "punch"
             bj(ch)
             id = ""
             time.sleep(2)
             break
-    for i in range(16,22) :
+
+    for i in range(16, 22):
         if f"'{username} `[{i}]`" in result or f"'{username} `[{i}]*`" in result:
             ch = "halt"
             bj(ch)
             id = ""
             time.sleep(2)
             break
+
     if 'You lost' in result:
         print("lost")
         ch = "lost"
@@ -122,17 +108,17 @@ while(start == True) :
         game = True
         id = ""
         list1 = []
-    elif 'You won' in result :
+    elif 'You won' in result:
         print("won")
         ch = "won"
-        bj(ch) 
+        bj(ch)
         game = True
         id = ""
         list1 = []
-    elif "~ You tied!'" in result or "~ You both bust!" in result :
+    elif "~ You tied!'" in result or "~ You both bust!" in result:
         print("draw")
         ch = "draw"
-        bj(ch) 
+        bj(ch)
         game = True
         id = ""
         list1 = []
